80fb21bf2aad4b856eff400a0978865ba16c0a75
manzil-infinity180 <rahulvs2809@gmail.com>
2024-10-08 23:39:46 +0530

    added search functionality

--- src/routers/bookmarkRoute.ts
+++ src/routers/bookmarkRoute.ts
@@ -8,20 +8,26 @@ import {
   getBookMark,
   getBookmarkByTopic,
   getMyProfile,
+  searchBookmark,
   updateBookmark,
   uploadBookmarkImage,
 } from "../controllers/bookmarkController.js";
 import { isAuthenticated } from "../controllers/authController.js";
 import { UploadSingleImage } from "../utils/UploadImages.js";
 router.use(isAuthenticated);
+/** GET  */
 router.get("/get-bookmark/:id", getBookMark);
 router.get("/get-all-bookmark", getAllBookMark);
-router.post("/get-bookmark-by-topics", getBookmarkByTopic);
 router.get("/get-my-profile", getMyProfile);
+/** POST */
+router.post("/get-bookmark-by-topics", getBookmarkByTopic);
 router.post("/create-bookmark", createNewBookmark);
+router.post("/search-bookmark", searchBookmark);
+/** PATCH */
 router.patch("/update-bookmark", updateBookmark);
-router.delete("/delete-bookmark/:id", deleteBookmark);
 router.patch("/add-bookmark-topic", addBookmarkTopic);
 router.patch("/upload-bookmark-image", UploadSingleImage, uploadBookmarkImage);
+/** DELETE */
+router.delete("/delete-bookmark/:id", deleteBookmark);
 
 export { router };
