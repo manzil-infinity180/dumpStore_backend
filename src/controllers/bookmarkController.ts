f61ba8688058b94e01a3d46035a2f795f90086bf
manzil-infinity180 <rahulvs2809@gmail.com>
2024-11-18 00:27:45 +0530

    refractor bookmark

--- src/controllers/bookmarkController.ts
+++ src/controllers/bookmarkController.ts
@@ -8,44 +8,27 @@ import { UploadImageToCloudinary } from "../utils/UploadImages.js";
 // const { google } = require("googleapis");
 import * as cheerio from "cheerio"
 import { google } from "googleapis";
+import { ErrorResponse, SuccessResponse, SuccessResponseWithoutData } from '../utils/controllerUtils.js';
 const getBookMark = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const bookmark = await Bookmark.findById(req.params.id);
-    res.status(200).json({
-      status: "sucess",
-      message: "Bookmark Data Fetched",
-      data: bookmark,
-    });
+    SuccessResponse(res,"Bookmark Data Fetched",200, bookmark);
   } catch (err) {
-    res.status(400).json({
-      status: "failed",
-      message: (err as Error).message,
-    });
+    ErrorResponse(res, err, 400);
   }
 };
 const getMyProfile = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    console.log(req.user);
     //@ts-ignore
     const user = await User.findById({ _id: req.user._id }).populate("posts");
-    console.log(user)
-    res.status(200).json({
-      status: "sucess",
-      message: "User Data Fetched",
-      data: user,
-    });
+    SuccessResponse(res,"User Data Fetched",200, user);
   } catch (err) {
-    res.status(400).json({
-      status: "failed",
-      message: (err as Error).message,
-    });
+    ErrorResponse(res, err, 400);
   }
 };
 const createNewBookmark = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { title, link, tag, calendar } = req.body;
-    console.log(calendar);
-    console.log(req.body);
     if (!title || !link || !tag) {
       throw new Error("Title, Link, Tag is compulsory fields");
     }
@@ -58,7 +41,6 @@ const createNewBookmark = async (req: Request, res: Response, next: NextFunction
     }
     //@ts-ignore
     const user = await User.findById({ _id: req.user._id });
-    console.log(user.posts);
     // TODO : if req.file is existed (manual logo) then you need to omit/override the image
     const bookmarkBody: IBookMark = {
       title,
@@ -71,32 +53,21 @@ const createNewBookmark = async (req: Request, res: Response, next: NextFunction
     };
     const bookmark = await Bookmark.create(bookmarkBody);
 
-    // console.log("user");
     user.posts.push(bookmark._id);
 
     if (req.body.topics) {
-      console.log(req.body.topics);
-      console.log(typeof req.body.topics);
       const result = user.topics.find(
         (el) => req.body.topics.toLowerCase() == el.toLowerCase()
       );
-      console.log(result);
       if (!result) {
         user.topics.push(req.body.topics);
       }
     }
     await user.save();
-    console.log(user);
-    res.status(200).json({
-      status: "sucess",
-      message: "New Dump Data is Inserted",
-      data: bookmark,
-    });
+    SuccessResponse(res,"New Dump Data is Inserted",200, bookmark);
+    
   } catch (err) {
-    res.status(400).json({
-      status: "failed",
-      message: (err as Error).message,
-    });
+    ErrorResponse(res, err, 400);
   }
 };
 /**
@@ -106,38 +77,23 @@ const getAllBookMark = async (req: Request, res: Response, next: NextFunction) =
   try {
     //@ts-ignore
     const allBookmark = await Bookmark.find({ user_id: req.user._id });
-    res.status(200).json({
-      status: "sucess",
-      message: "User all Bookmark Data",
-      data: allBookmark,
-    });
+    SuccessResponse(res,"User all Bookmark Data",200, allBookmark);
   } catch (err) {
-    res.status(400).json({
-      status: "failed",
-      message: (err as Error).message,
-    });
+    ErrorResponse(res, err, 400);
   }
 };
 
 const getBookmarkByTopic = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    console.log(req.body.topics);
     //@ts-ignore
     const user = await User.findById(req.user._id);
     const allBookmark = await Bookmark.find({
       topics: req.body.topics,
       user_id: user._id,
     });
-    res.status(200).json({
-      status: "sucess",
-      message: "Updated Bookmark Data",
-      data: allBookmark,
-    });
+    SuccessResponse(res,"Updated Bookmark Data",200, allBookmark);
   } catch (err) {
-    res.status(400).json({
-      status: "failed",
-      message: (err as Error).message,
-    });
+    ErrorResponse(res, err, 400);
   }
 };
 // const getBookMarkByTags = async (req: Request, res: Response, next: NextFunction) => {
@@ -159,66 +115,44 @@ const getBookmarkByTopic = async (req: Request, res: Response, next: NextFunctio
 const updateBookmark = async (req: Request, res: Response, next: NextFunction) => {
   try {
     // const { isChecked, image } = req.body;
-    console.log(req.body);
     const { isChecked, image, topics } = req.body;
-    console.log(isChecked);
     if (isChecked && isChecked.toLowerCase() === "yes" && image.includes("cloudinary")) {
       const domain = new URL(req.body.link).hostname;
       if (!process.env.LOGO_FAVICON_URL) throw new Error("Favicon URl is invalid");
       const favicon = process.env.LOGO_FAVICON_URL.replace("<DOMAIN>", domain);
       req.body.image = favicon;
-      console.log(req.body);
     }
 
     const Updatebookmark = await Bookmark.findByIdAndUpdate(
       { _id: req.body.id },
       req.body
     );
-    console.log(req.body);
     const x = await Bookmark.findById(req.body.id);
     // @ts-ignore
     const user = await User.findById({ _id: req.user._id });
-    // console.log("user");
     if (topics !== undefined && topics.length) {
-      console.log(topics);
-      console.log(user.topics);
       const result = user.topics.some((el) => {
         return topics.toLowerCase() === el.toLowerCase();
       });
-      console.log(result);
       if (!result) {
         user.topics.push(req.body.topics);
         await user.save();
       }
     }
-
-    console.log(x);
-    res.status(200).json({
-      status: "sucess",
-      message: "Updated Bookmark Data",
-    });
+    SuccessResponseWithoutData(res, "Updated Bookmark Data", 200);
   } catch (err) {
-    console.log(err);
-    res.status(400).json({
-      status: "failed",
-      message: (err as Error).message,
-    });
+    ErrorResponse(res, err, 400);
   }
 };
 
 const updateBookmarkOrder = async (req: Request, res: Response) => {
   const { updatedOrder } = req.body;
-  console.log(updatedOrder);
   try {
     for (let item of updatedOrder) {
       const { _id: _, ...rest } = item;
-      console.log(rest);
       await Bookmark.findByIdAndUpdate(item._id, rest);
     }
-    res.status(200).json({
-      status: "success",
-      data: "Order Successfully Saved",
-    });
+    SuccessResponseWithoutData(res, "Order Successfully Saved", 200);
   } catch (error) {
     console.error("Error updating order:", error);
     res.status(500).send("Failed to update order");
@@ -237,15 +171,9 @@ const deleteBookmark = async (req: Request, res: Response, next: NextFunction) =
     filterOrder.map((el) => newpost.push(el._id));
     user.posts = newpost;
     user.save();
-    res.status(200).json({
-      status: "sucess",
-      message: "Deleted Bookmark Data",
-    });
+    SuccessResponseWithoutData(res, "Deleted Bookmark Data", 200);
   } catch (err) {
-    res.status(400).json({
-      status: "failed",
-      message: (err as Error).message,
-    });
+    ErrorResponse(res, err, 400);
   }
 };
 const deleteAllBookmarkByTopics = async (
@@ -268,16 +196,9 @@ const deleteAllBookmarkByTopics = async (
       user.topics = filterTopics;
       await user.save();
     }
-
-    res.status(200).json({
-      status: "sucess",
-      message: "Deleted Bookmark Data",
-    });
+    SuccessResponseWithoutData(res, "Deleted Bookmark Data", 200);
   } catch (err) {
-    res.status(400).json({
-      status: "failed",
-      message: (err as Error).message,
-    });
+    ErrorResponse(res, err, 400);
   }
 };
 
@@ -289,16 +210,9 @@ const addBookmarkTopic = async (req: Request, res: Response, next: NextFunction)
     }
     const { topics }: Pick<IBookMark, "topics"> = req.body;
     const addTopics = await Bookmark.findByIdAndUpdate({ _id: req.body.id }, { topics });
-
-    res.status(200).json({
-      status: "sucess",
-      message: `Added topic ${topics} to bookmark`,
-    });
+    SuccessResponseWithoutData(res, `Added topic ${topics} to bookmark`, 200);
   } catch (err) {
-    res.status(400).json({
-      status: "failed",
-      message: (err as Error).message,
-    });
+    ErrorResponse(res, err, 400);
   }
 };
 
@@ -317,17 +231,10 @@ const uploadBookmarkImage = async (req: Request, res: Response, next: NextFuncti
     const bookmark = await Bookmark.findById(req.query.id);
     bookmark.image = result.secure_url;
     await bookmark.save();
-    res.status(200).json({
-      status: "success",
-      message: "Uploaded Image to Cloudinary",
-      data: bookmark,
-      newImage: bookmark.image,
-    });
+    // Optional Parameter which i am passing previously - newImage: bookmark.image,
+    SuccessResponse(res,"Uploaded Image to Cloudinary",200, bookmark);
   } catch (err) {
-    res.status(400).json({
-      status: "failed",
-      message: (err as Error).message,
-    });
+    ErrorResponse(res, err, 400);
   }
 };
 
@@ -340,16 +247,10 @@ const uploadImageToCloud = async (req: Request, res: Response, next: NextFunctio
     if (result.length === 0 && !result.secure_url) {
       throw new Error("Failed to Upload Image");
     }
-    console.log(result);
-    res.status(200).json({
-      status: "success",
-      data: { imageUrl: result.secure_url },
-    });
+    const imageUrl = result.secure_url;
+    SuccessResponse(res,"Uploaded Image to Cloudinary",200, {imageUrl});
   } catch (err) {
-    res.status(400).json({
-      status: "failed",
-      message: (err as Error).message,
-    });
+    ErrorResponse(res, err, 400);
   }
 };
 export const oauth2Client = new google.auth.OAuth2(
@@ -364,7 +265,6 @@ const addRemainderToCalendar = async (
 ) => {
   try {
     const {summary, link, endDate, startDate} = req.body;
-    console.log(req.body);
     const calendar = google.calendar({ version: "v3", auth: oauth2Client });
     const event = {
       summary: summary,
@@ -385,7 +285,6 @@ const addRemainderToCalendar = async (
         ],
       },
     };
-    console.log(event);
     const result = await calendar.events.insert({
       calendarId: "primary", // Use the primary calendar of the user
       requestBody: event,
@@ -398,11 +297,7 @@ const addRemainderToCalendar = async (
       },
     });
   } catch (err) {
-    console.log(err);
-    res.status(400).json({
-      status: "failed",
-      message: (err as Error).message,
-    });
+    ErrorResponse(res, err, 400);
   }
 };
 const updateRemainderToCalendar = async (
@@ -416,7 +311,6 @@ const updateRemainderToCalendar = async (
     if(!eventId){
       throw new Error("EventId is missing");
     }
-    console.log(req.body);
     const calendar = google.calendar({ version: "v3", auth: oauth2Client });
     const event = {
       summary: summary,
@@ -437,7 +331,6 @@ const updateRemainderToCalendar = async (
         ],
       },
     };
-    console.log(event);
     const result = await calendar.events.patch({
       calendarId: "primary", // Use the primary calendar of the user
       requestBody: event,
@@ -465,11 +358,7 @@ const updateRemainderToCalendar = async (
       message:"Updated Your Remainder"
     });
   } catch (err) {
-    console.log(err);
-    res.status(400).json({
-      status: "failed",
-      message: (err as Error).message,
-    });
+    ErrorResponse(res, err, 400);
   }
 };
 
@@ -484,7 +373,6 @@ const deleteRemainder = async (
     if(!eventId){
       throw new Error("EventId is missing");
     }
-    console.log(req.body);
     const calendar = google.calendar({ version: "v3", auth: oauth2Client });
     const result = await calendar.events.delete({
       calendarId: "primary", // Use the primary calendar of the user
@@ -493,16 +381,9 @@ const deleteRemainder = async (
     const bookmarkCalendarData = await Bookmark.updateOne({_id: bookmarkId}, {
       $unset:{calendar: ""}
     });
-    res.status(200).json({
-      status: "success",
-      message:"Deleted remainder successfully"
-    });
+    SuccessResponseWithoutData(res, "Deleted remainder successfully", 200);
   } catch (err) {
-    console.log(err);
-    res.status(400).json({
-      status: "failed",
-      message: (err as Error).message,
-    });
+    ErrorResponse(res, err, 400);
   }
 };
 
@@ -528,25 +409,20 @@ const searchBookmark = async (req: Request, res: Response, next: NextFunction) =
         },
       },
     ]);
-    console.log(result);
 
     res.status(200).json({
       status: "success",
-      message: "Uploaded Image to Cloudinary",
+      message: "Search Result",
       length: result.length,
       data: result,
     });
   } catch (err) {
-    res.status(400).json({
-      status: "failed",
-      message: (err as Error).message,
-    });
+    ErrorResponse(res, err, 400);
   }
 };
 const getAllChromeBookmark = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const file = req.file;
-    console.log(file);
     if (!file) {
       throw new Error("No file received");
     }
@@ -581,21 +457,13 @@ const getAllChromeBookmark = async (req: Request, res: Response, next: NextFunct
       bookmarks.push(bookmark_object);
     });
 
-    // console.log(bookmarks.slice(0, 5));
     const uploadBookmark = await Bookmark.insertMany(bookmarks);
-    console.log(uploadBookmark);
     if (!uploadBookmark) {
       throw new Error("Getting issue while uploading to database");
     }
-    // user.posts.push(bookmark._id);
-    // uploadBookmark.forEach((el) => {
-    //   user.posts.push(el._id);
-    // });
-
     const result = user.topics.find(
       (el) => "chrome".toLowerCase() == el.toLowerCase()
     );
-    console.log(result);
     if (!result) {
       user.topics.push("chrome");
     }
@@ -603,15 +471,12 @@ const getAllChromeBookmark = async (req: Request, res: Response, next: NextFunct
 
     res.status(200).json({
       status: "success",
-      message: "Uploaded bro",
+      message: "All bookmark fetched",
       totalBookmarkInserted: uploadBookmark.length,
       // allBookmark: bookmarks,
     });
-  } catch (error) {
-    res.status(400).json({
-      status: "failed",
-      message: (error as Error).message,
-    });
+  } catch (err) {
+    ErrorResponse(res,err,400);
   }
 };
 
@@ -620,17 +485,11 @@ const getAllChromeBookmarkFromExtension =async (req: Request, res: Response, nex
     const user = await User.findById(req.user);
     
     const {allbookmark} = req.body;
-    // if(allbookmark.length >= 1){
-    //   throw new Error("No data arrived");
-    // }
     const x : Partial<IBookMark>[] = allbookmark
-    console.log(x);
-    // console.log(allbookmark);
     const uploadBookmark = await Bookmark.insertMany(allbookmark as Partial<IBookMark>[]);
     const result = user.topics.find(
       (el) => "chrome".toLowerCase() == el.toLowerCase()
     );
-    console.log(result);
     if (!result) {
       user.topics.push("chrome");
     }
@@ -643,10 +502,7 @@ const getAllChromeBookmarkFromExtension =async (req: Request, res: Response, nex
       // allBookmark: bookmarks,
     });
   }catch(err){
-      res.status(400).json({
-          status: "failed",
-          message: (err as Error).message,
-      });
+      ErrorResponse(res,err,400);
   }
 }
 // TODO : get bookmark by topic and set default as all
